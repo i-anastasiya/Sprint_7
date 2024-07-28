8362bdd0788698a56e604992547e0fb86c003c89
Nastya Ivchenko <ivchenko.nastya@yandex.ru>
2024-07-28 19:44:31 +0300

    Финальный проект спринта 7

--- datails.py
+++ datails.py
@@ -4,14 +4,12 @@ class Url:
     RECEIVING_ORDERS = 'https://qa-scooter.praktikum-services.ru/api/v1/orders'
     LOGIN = 'https://qa-scooter.praktikum-services.ru/api/v1/courier/login'
     DELETE_COURIER = 'https://qa-scooter.praktikum-services.ru/api/v1/courier/login'
-
 class Text:
     USER_CREATION_SUCCESS = '{"ok":true}'
     ACCOUNT_NOT_FOUND = "Учетная запись не найдена"
     INSUFFICIENT_DATA_FOR_ACCOUNTING = "Недостаточно данных для входа"
     INSUFFICIENT_DATA_CREATE_USER = "Недостаточно данных для создания учетной записи"
     LOGIN_ALREADY_USAGE = "Этот логин уже используется. Попробуйте другой."
-
 class DatailsUsers:
     datails_user = [
         ['Иван', 'Иванов', 'ул. Красная площадь, д. 3', '4', '+7950000000', '5', '2020-06-06',
